65220bfe9dd704c22701a8361c78e49179835ec3
shyook <footprintz@daum.net>
2021-05-09 11:10:35 +0900

    from chapter1 to chapter2

--- test/test.py
+++ test/test.py
@@ -1,13 +1,14 @@
-'''
+import re
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+'''
 html = urlopen("http://pythonscraping.com/pages/page1.html")
 print(html.read())
 '''
 
 '''
-from urllib.request import urlopen
-from urllib.request import HTTPError
-from bs4 import BeautifulSoup
 try:
     html = urlopen("http://pythonscraping.com/pages/page1.html")
 except HTTPError as e:
@@ -17,3 +18,55 @@ else:
     print(bsObj.h1)
     print(bsObj.head)
 '''
+
+'''
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+
+    return title
+
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
+'''
+
+'''
+url = "http://pythonscraping.com/pages/warandpeace.html"
+html = urlopen(url)
+bsObj = BeautifulSoup(html, "html.parser")
+
+greenList = bsObj.findAll("span", {"class":"green"})
+for name in greenList:
+    print(name.get_text())
+
+print(bsObj.div.span)
+'''
+
+url = "http://pythonscraping.com/pages/page3.html"
+html = urlopen(url)
+bsObj = BeautifulSoup(html, "html.parser")
+
+#for child in bsObj.find("table", {"id":"giftList"}).children:
+#    print(child)
+
+#for sibling in bsObj.find("table", {"id":"giftList"}).tr.next_siblings:
+#    print(sibling)
+
+#print(bsObj.find("img", {"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
+
+#images =  bsObj.findAll("img", {"src":re.compile("\.\.\/img/gifts/img.*\.jpg")})
+#for image in images:
+#    print(image["src"])
+
+print(bsObj.findAll(lambda tag: len(tag.attrs)==2))
